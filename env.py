ed92911d3348fc8c7e558b54a3368714f206185d
sthDINESH <sthapit.dnsh@gmail.com>
2026-09-07 16:59:46 +0100

    add steps to extract species detail from api and add to mongodb

--- env.py
+++ env.py
@@ -2,9 +2,11 @@ import os
 
 from dotenv import load_dotenv
 
-
 load_dotenv()
 PERENUAL_API_KEY = os.getenv("PERENUAL_API_KEY")
+
+# URI for mongoDB database
+MONGODB_URI = 'mongodb://localhost:27017/'
 
 PERENUAL_ENDPOINT = 'https://perenual.com/api/v2/'
 SPECIES_API = PERENUAL_ENDPOINT + 'species-list'
--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import api_requests as api
-import pprint as pp
-from env import SPECIES_API
+from env import MONGODB_URI, SPECIES_API, SPECIES_DETAIL_API
 from mongo_db_driver import get_db, get_collection
+import api_requests as api
 
-
-MONGODB_URI = 'mongodb://localhost:27017/'
-# Connect to database
+# Connect to MongoDB
 db = get_db(MONGODB_URI, "plants_101")
-species_collection = get_collection(db, "species")
 
-# Fetch the list first so the detail endpoint receives a numeric species ID.
+# Get relevant collections within the database
+species_collection = get_collection(db, "species")
+species_detail_collection = get_collection(db, "species_detail")
+
+# Fetch plant species list from API and add it to database
 species = api.get(SPECIES_API)
-print("SPECIES DICT")
-pp.pprint(species)
 
 if "data" in species:
     for plant in species["data"]:
@@ -24,6 +22,41 @@ if "data" in species:
     print(f"✓ Added {len(species['data'])} species to the collection.")
 else:
     print(f"✗ Could not add species data: {species}")
+    exit(1)
+
+# Fetch details for each species from API and add it to database
+for plant in species_collection.find():
+    print(f"Fetching data for {plant['common_name']}")
+    species_detail_url = SPECIES_DETAIL_API + str(plant["id"])
+    species_detail = api.get(species_detail_url)
+    if "error" in species_detail:
+        print(
+            f"✗ Could not fetch details for {plant['common_name']}: "
+            f"{species_detail['error']}"
+        )
+        continue
+
+    # Add detail to mondodb to create the document in species_detail_collection
+    species_detail_collection.replace_one(
+        {"id": plant["id"]},
+        species_detail,
+        upsert=True,
+    )
+
+    # Get the created document
+    detail_document = species_detail_collection.find_one(
+        {"id": plant["id"]}
+    )
+
+    # Add reference to the document in species_collection documents
+    species_collection.update_one(
+        {"_id": plant["_id"]},
+        {"$set": {"species_detail_id": detail_document["_id"]}},
+    )
+    print(f"✓ Saved details for {plant['common_name']}")
+
+
+
 
 
 
